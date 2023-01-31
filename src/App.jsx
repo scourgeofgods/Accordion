f3cc1c44d9eaaceb272f36d0ec6ec61e9bb00a1b
Murat Arslantas <murataslanaslan@outlook.com>
2023-01-31 20:47:11 +0300

    fixed align text

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ function App() {
     <div className="acc py-5">
       <div className=" vh-100 container ">
         <div className="row justify-content-center align-items-center ">
-          <div className="col-md-4 p-2">
+          <div className="col-md-6 p-2">
             <h1 className="text-uppercase text-center">
               frequently asked questions
             </h1>
           </div>
         </div>
-        <div className="row h-75 align-items-center justify-content-between rounded-3 p-2 ">
-          <div className="col-lg-4 col-12  ">
-            <h1 className="text-capitalize text-md-start text-center">
+        <div className="row  align-items-center justify-content-between rounded-3 p-2 ">
+          <div className="col-lg-4 col-12 ">
+            <h1 className="text-capitalize text-lg-start text-center">
               questions and answers about login
             </h1>
           </div>
